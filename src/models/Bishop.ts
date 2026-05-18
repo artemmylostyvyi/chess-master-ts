bfb5be4998e6f8ce0926096a112037b3be56504c
ipz244_shao <ipz244_shao@student.ztu.edu.ua>
2026-05-19 01:22:47 +0300

    fix: replace constructor.name with explicit type property in pieces

--- src/models/Bishop.ts
+++ src/models/Bishop.ts
@@ -3,6 +3,7 @@ import {Color} from "./types.js";
 import {BishopMoveStrategy} from "./strategies/BishopMoveStrategy.js";
 
 export class Bishop extends Piece{
+    public readonly type = "Bishop";
     constructor(position: {x: number, y: number}, color: Color) {
         super(position, color, new BishopMoveStrategy());
     }
--- src/models/Queen.ts
+++ src/models/Queen.ts
@@ -4,6 +4,7 @@ import {DefaultMoveStrategy} from "./strategies/DefaultMoveStrategy.js";
 import {QueenMoveStrategy} from "./strategies/QueenMoveStraregy.js";
 
 export class Queen extends Piece{
+    public readonly type = "Queen";
     constructor(position: {x: number, y: number}, color: Color) {
         super(position, color, new QueenMoveStrategy());
     }
